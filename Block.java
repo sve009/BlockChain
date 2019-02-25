ed3888248ef2f27c2dd14e310ae560348f9d7229
Sam Eagen <sve9milo@gmail.com>
2019-02-25 13:00:43 -0600

    Added class templates

---
+++ Block.java
@@ -0,0 +1,44 @@
+public class Block {
+        private int num;
+        private int data;
+        private Hash prevHash;
+        private long nonce;
+        private Hash hash;
+
+        public Block(int num, int amount, Hash prevHash) {
+               this.num = num;
+               this.data = amount;
+               this.prevHash = prevHash;
+        }
+
+        public Block(int num, int amount, Hash prevHash, long nonce) {
+                this.num = num;
+                this.data = amount;
+                this.prevHash = prevHash;
+                this.nonce = nonce;
+        }
+
+        public int getNum() {
+                return this.num;
+        }
+
+        public int getAmount() {
+                return this.data;
+        }
+
+        public long getNonce() {
+                return this.nonce;
+        }
+
+        public Hash getPrevHash() {
+                return this.prevHash;
+        }
+
+        public Hash getHash() {
+                return this.hash;
+        }
+
+        public String toString() {
+                //Implement
+        }
+}
---
+++ Hash.java
@@ -0,0 +1,23 @@
+public class Hash {
+        private byte[] data;
+
+        public Hash(byte[] data) {
+                this.data = data;
+        }
+
+        public byte[] getData() {
+                return this.data;
+        }
+
+        public boolean isValid() {
+                //Implement
+        }
+
+        public String toString() {
+                //Implement
+        }
+
+        public boolean equals(Object other) {
+                //Implement
+        }
+}
