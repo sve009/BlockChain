5767ba6cc7d4427bd9e0c091ed6af035d9627f95
Sam Eagen <sve9milo@gmail.com>
2019-02-26 13:14:27 -0600

    BlockChain outline

--- BlockChain.java
+++ BlockChain.java
@@ -1,4 +1,40 @@
 
 public class BlockChain {
         //Implement datastructure
+       
+        public BlockChain(int initial) {
+               //Implement 
+        }
+
+        public Block mine(int amount) {
+               //Implement
+        } 
+
+        public int getSize() {
+                //Implement
+        }
+
+        public void append(Block blk) {
+                //Implement
+        }
+
+        public boolean removeLast() {
+                //Implement
+        }
+
+        public Hash getHash() {
+                //Implement
+        }
+
+        public boolean isValidBlock() {
+                //Implement
+        }
+
+        public void printBalances() {
+
+        }
+
+        public String toString() {
+
+        }
 }
