c4869a12f5e12007551160c9e0d01e63fa8873d2
syamala <gumidyal@grinnell.edu>
2019-02-26 13:18:48 -0600

    changes

--- BlockChain.java
+++ BlockChain.java
@@ -1,4 +1,20 @@
 
 public class BlockChain {
-        //Implement datastructure
+
+
+  public static class Node {
+    public Node head;
+    public int count;
+
+    public Node(Object object) {
+      head = new Node(null);
+      count = 0;
+    }
+
+  }
+  
+
+
+
+
 }
