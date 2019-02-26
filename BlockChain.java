6d1a258cbe3faa199121515f66e4f38a2ec99fd1
Sam Eagen <sve9milo@gmail.com>
2019-02-26 15:00:12 -0600

    Changed BlockChain

--- BlockChain.java
+++ BlockChain.java
@@ -1,8 +1,6 @@
 
 public class BlockChain {
 
-
-
   public static class Node {
     public Node head;
     public int count;
@@ -11,50 +9,44 @@ public class BlockChain {
       head = new Node(null);
       count = 0;
     }
+  }
+
+  //Implement datastructure
+
+  public BlockChain(int initial) {
+    //Implement 
+  }
+
+  public Block mine(int amount) {
+    //Implement
+  } 
+
+  public int getSize() {
+    //Implement
+  }
+
+  public void append(Block blk) {
+    //Implement
+  }
+
+  public boolean removeLast() {
+    //Implement
+  }
+
+  public Hash getHash() {
+    //Implement
+  }
+
+  public boolean isValidBlock() {
+    //Implement
+  }
+
+  public void printBalances() {
 
   }
-  
 
+  public String toString() {
 
-
-
-
-        //Implement datastructure
-       
-        public BlockChain(int initial) {
-               //Implement 
-        }
-
-        public Block mine(int amount) {
-               //Implement
-        } 
-
-        public int getSize() {
-                //Implement
-        }
-
-        public void append(Block blk) {
-                //Implement
-        }
-
-        public boolean removeLast() {
-                //Implement
-        }
-
-        public Hash getHash() {
-                //Implement
-        }
-
-        public boolean isValidBlock() {
-                //Implement
-        }
-
-        public void printBalances() {
-
-        }
-
-        public String toString() {
-
-        }
+  }
 
 }
