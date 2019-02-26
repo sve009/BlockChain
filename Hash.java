e51fbcd4dcdc8c45ef3cbb6a41060c924e20d316
Sam Eagen <sve9milo@gmail.com>
2019-02-26 12:56:45 -0600

    Implemented stuff

--- Hash.java
+++ Hash.java
@@ -10,11 +10,24 @@ public class Hash {
         }
 
         public boolean isValid() {
-                //Implement
+                String str = this.toString();
+
+                for (int i = 0; i < 3; i++) {
+                        if (str.charAt(i) != 0)  {
+                                return false;
+                        }
+                }
+
+                return true;
         }
 
         public String toString() {
                 String retString = new String();
+                for (byte b: this.data) {
+                        retString += String.format("%x", Byte.toUnsignedInt(b));
+                }
+
+                return retString;
         }
 
         public boolean equals(Object other) {
