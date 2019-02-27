fc8f7774bce2fbcf4a5bf6a46be45b2a2a86322d
Sam Eagen <sve9milo@gmail.com>
2019-02-26 21:07:26 -0600

    Added hash creation to Block constructor

--- Block.java
+++ Block.java
@@ -1,28 +1,41 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.nio.ByteBuffer;
 
 public class Block {
         private int num;
         private int data;
         private Hash prevHash;
-        private long nonce;
+        private long nonce = -1;
         private Hash hash;
 
-        public Block(int num, int amount, Hash prevHash) {
+        public Block(int num, int amount, Hash prevHash) throws NoSuchAlgorithmException {
                this.num = num;
                this.data = amount;
                this.prevHash = prevHash;
 
                //Generate hash
+               boolean nonceFound = false;
+               long nonce = -1;
+
+               while (this.nonce == -1) {
+                       nonce++;
+                       Hash hash = new Hash(Block.calculateHash(num, amount, prevHash, nonce));
+                       if (hash.isValid()) {
+                             this.nonce = nonce; 
+                             this.hash = hash;
+                       }
+               }
         }
 
-        public Block(int num, int amount, Hash prevHash, long nonce) {
+        public Block(int num, int amount, Hash prevHash, long nonce) throws NoSuchAlgorithmException {
                 this.num = num;
                 this.data = amount;
                 this.prevHash = prevHash;
                 this.nonce = nonce;
 
                 //Generate hash
+                this.hash = new Hash(Block.calculateHash(num, amount, prevHash, nonce));
         }
 
         public int getNum() {
@@ -47,5 +60,26 @@ public class Block {
 
         public String toString() {
                 //Implement
+        }
+
+        public static byte[] calculateHash(int num, int data, long nonce) throws NoSuchAlgorithmException {
+                MessageDigest md = MessageDigest.getInstance("sha-256");
+
+                md.update(ByteBuffer.allocate(4).putInt(num).array());
+                md.update(ByteBuffer.allocate(4).putInt(data).array());
+                md.update(ByteBuffer.allocate(4).putLong(nonce).array());
+
+                return md.digest();
+        }
+
+        public static byte[] calculateHash(int num, int data, Hash prevHash, long nonce) throws NoSuchAlgorithmException {
+                MessageDigest md = MessageDigest.getInstance("sha-256");
+
+                md.update(ByteBuffer.allocate(4).putInt(num).array());
+                md.update(ByteBuffer.allocate(4).putInt(data).array());
+                md.update(prevHash.getData());
+                md.update(ByteBuffer.allocate(4).putLong(nonce).array());
+
+                return md.digest();
         }
 }
